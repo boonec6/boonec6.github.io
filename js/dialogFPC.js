9a2b41c7cd0877c68545ada7526b2f8f562ba6ef
xps <xps@boone.boone>
2019-08-30 13:38:05 -0500

    FPC

--- js/dialogFPC.js
+++ js/dialogFPC.js
@@ -14,9 +14,18 @@
         var param = tableau.extensions.settings.get("param");
 
         let dashboard = tableau.extensions.dashboardContent.dashboard;
+
+        dashboard.getParametersAsync().then(function(params) {
+            params.forEach(function(p) {
+                $('#param').append("<option value='" + p.id + "'>" + p.name + "</option>");
+            })
+        })
+
         dashboard.worksheets.forEach(function(worksheet) {
             $("#srcSheet").append("<option value='" + worksheet.name + "'>" + worksheet.name + "</option>");
         });
+
+
         var worksheetName = tableau.extensions.settings.get("worksheet");
         if (worksheetName != undefined) {
             $("#srcSheet").val(worksheetName);
@@ -45,7 +54,7 @@
         var counter = 1;
         worksheet.getFiltersAsync().then(function(filters) {
             filters.forEach(function(f) {
-                $("#srcFilter").append("<option value='" + counter + "'>" + f.fieldId + "</option>");
+                $("#srcFilter").append("<option value='" + f.fieldId + "'>" + f.fieldName + "</option>");
                 counter++;
             });
         });
