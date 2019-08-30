5c9f9bc3b176e68610e5446fbf47d8b34df45cb5
xps <xps@boone.boone>
2019-08-30 14:16:30 -0500

    sFPC

--- js/simpleFilterParamChange.js
+++ js/simpleFilterParamChange.js
@@ -2,7 +2,7 @@
 
 $(document).ready(function() {
     tableau.extensions.initializeAsync({ 'configure': configure }).then(function() {
-        //createFilterListener();
+        createFilterListener();
     }, function() { console.log('Error while Initializing: ' + err.toString()); });
 });
 
@@ -22,34 +22,25 @@ function configure() {
 }
 
 function createFilterListener() {
-    var wsN = 'Title';
+    var srcSheet = tableau.extensions.settings.get("srcSheet");
+    var srcField = tableau.extensions.settings.get("srcField");
+    var param = tableau.extensions.settings.get("param");
+    console.log(srcSheet);
+    console.log(srcField);
+    console.log(param);
     const wss = tableau.extensions.dashboardContent.dashboard.worksheets;
     var ws = wss.find(function(sheet) {
-        return sheet.name === wsN;
+        return sheet.name === srcSheet;
     });
     ws.addEventListener(tableau.TableauEventType.FilterChanged, (filterEvent) => {
-        //showLoading().then(() => {
-        //alert("showLoading done");
         tableau.extensions.dashboardContent.dashboard.getParametersAsync().then(function(t) {
-                var selectedLead = t.find(p1 => p1.name === "selectedLead");
-                var selectedProject = t.find(p2 => p2.name === "selectedProjectLead");
-                //alert('changed');
+            var selParam = t.find(p1 => p1.name === param);
 
-                ws.getSummaryDataAsync().then(function(sumdata) {
-                        var l1 = sumdata.columns.find(lll => lll.fieldName === "Id").index;
-                        var l1a = sumdata.columns.find(llla => llla.fieldName === "ProjectLead").index;
-                        var l2 = (sumdata.data[0][l1]._value);
-                        var l2a = (sumdata.data[0][l1a]._value);
-                        selectedLead.changeValueAsync(l2);
-                        selectedProject.changeValueAsync(l2a);
-                        //refreshData();
-                    })
-                    // .then(function(x) {
-                    //     //alert("sumdataAsync complete");
-                    //     hideLoading();
-                    // })
+            ws.getSummaryDataAsync().then(function(sumdata) {
+                var value = sumdata[srcField][0];
+                console.log(value);
+                selParam.changeValueAsync(value);
             })
-            //});
-
+        })
     });
 };
